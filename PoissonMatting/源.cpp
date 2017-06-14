346f42c20b1287c6d2ffb88cc3707fea3e982ac1
prayshouse <3140103730@zju.edu.cn>
2017-06-14 20:27:14 +0800

    A way to create transparent pixels

--- PoissonMatting/源.cpp
+++ PoissonMatting/源.cpp
@@ -1,13 +1,34 @@
 #include <opencv.hpp>
+#include <string>
+#include <vector>
 
 using namespace std;
 using namespace cv;
 
 int main()
 {
-	Mat src = imread("A.png");
-	imshow("A", src);
+	Mat src = imread("troll.png");
+	Mat trimap = imread("trollTrimap");
+	Mat test = Mat::zeros(50, 50, CV_8UC4);
+	//createAlphaMat(test);
+	string str = "./";
+	for (int i = 0; i < test.rows; i++)
+	{
+		for (int j = 0; j < test.cols; j++)
+		{
+			/*test.at<Vec4b>(i, j)[3] = 0;
+			test.at<Vec4b>(i, j)[2] = 120;
+			test.at<Vec4b>(i, j)[1] = 255;
+			test.at<Vec4b>(i, j)[0] = 255;*/
+		}
+	}
 
+	//imshow("A", src);
+	vector<int> compression_params;
+	compression_params.push_back(IMWRITE_PNG_COMPRESSION);
+	compression_params.push_back(9);
+	imwrite("empty.png", test, compression_params);
+	imshow("empty", test);
 	waitKey(0);
 	return 0;
 }
